20d3b811813168fdfbec1d2737c0af823937b3b7
damatugit <damatugithub@gmail.com>
2025-12-11 00:44:32 -0300

    feat: interface created

---
+++ DocumentsService/src/interfaces/IDocumentRepository.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DocumentsService.src.models;
+
+namespace DocumentsService.src.interfaces
+{
+    /// <summary>
+    /// IDocumentRepository
+    /// </summary> <summary>
+    /// Interfaz que declara los metodos para el repositorio de documentos
+    /// </summary>
+    public interface IDocumentRepository
+    {
+         Task<Document> CreateAsync(Document doc);
+        Task<Document> GetByIdAsync(Guid id);
+        Task<IEnumerable<Document>> GetByWorkspaceAsync(Guid workspaceId);
+        Task<Document> UpdateContentAsync(Guid id, string contentJson);
+        Task<bool> SoftDeleteAsync(Guid id);
+    }
+}
