719f91897f13ce775443d5efc7da00bb59e77eaf
damatugit <damatugithub@gmail.com>
2025-12-12 20:53:28 -0300

    fix: DataSeeder adjusted to only 3 documents

--- DocumentsService/src/data/DataSeeder.cs
+++ DocumentsService/src/data/DataSeeder.cs
@@ -56,39 +56,7 @@ namespace DocumentsService.src.data
 
             // Insertar documentos fijos
             foreach (var doc in fixedDocs)
-                repo.SeedDocument(doc);
-
-            // ====== SEMILLA ALEATORIA ======
-            var workspaceIds = new[]
-            {
-                Guid.Parse("d3a1d94c-1f3e-445f-8412-7209c7c2af1b"),
-                Guid.Parse("d3a1d94c-1f3e-445f-8412-7209c7c2af1a"),
-                Guid.Parse("d3a1d94c-1f3e-445f-8412-7209c7c2af1c")
-            };
-
-            var userIds = new[]
-            {
-                Guid.Parse("a08799f8-746f-46b4-8134-2ef211fe705a"),
-                Guid.Parse("9fd8ec52-3aa4-4097-86fa-2c576bc06e01"),
-                Guid.Parse("2db519ca-4836-4e01-977f-ec518a081d54")
-            };
-
-            // 10 documentos aleatorios
-            for (int i = 0; i < 10; i++)
-            {
-                var doc = new Document
-                {
-                    Id = Guid.NewGuid(),
-                    WorkspaceId = faker.PickRandom(workspaceIds),
-                    Title = faker.Lorem.Sentence(),
-                    ContentJson = $"[{{\"type\":\"text\",\"content\":\"{faker.Lorem.Paragraph()}\"}}]",
-                    CreatedAt = faker.Date.Past(),
-                    UpdatedAt = faker.Date.Recent(),
-                    IsDeleted = false
-                };
-
-                repo.SeedDocument(doc);
-            }
+                repo.SeedDocument(doc);            
         }
     }
 }
